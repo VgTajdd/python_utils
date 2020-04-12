950b64e3e4f6c542c4d4f5e3c7692f24f6c9017f
VgTajdd <aduranddiaz@gmail.com>
2020-04-12 11:21:33 -0500

    Update python_home

--- scripts/install_requirements_in_venv.bat
+++ scripts/install_requirements_in_venv.bat
@@ -1,9 +1,7 @@
-::@echo off
+@echo off
 setlocal
-set CURRENT_DIR=%~dp0/../
-::set SWIG_PATH=C:\dev\third_party\swigwin-3.0.12
-::set PATH=%SWIG_PATH%;%PATH%;
-call "%CURRENT_DIR%/venv/Scripts/activate.bat"
+set CURRENT_DIR=%~dp0..\
+call "%CURRENT_DIR%venv\Scripts\activate.bat"
 call pip install -r requirements.txt
 call deactivate
 cmd \k
