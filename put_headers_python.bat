abc69ed6832eb86f56a5774a78a8c5f89ae743e4
VgTajdd <aduranddiaz@gmail.com>
2022-03-17 18:17:59 -0500

    Update put_headers_python.bat

--- put_headers_python.bat
+++ put_headers_python.bat
@@ -2,7 +2,7 @@
 setlocal
 set CURRENT_DIR=%~dp0
 call "%CURRENT_DIR%/venv/Scripts/activate.bat"
-call python put_headers_python.py "%CURRENT_DIR%/dir"
+call python put_headers_python.py "%CURRENT_DIR%dir"
 call deactivate
 endlocal
 cmd /k
